895b80550b89ef3a8f0e9089f11ad0628032f63e
12voltman <schuhplattler42@gmail.com>
2017-09-24 09:11:26 -0400

    Added template Secrets.h file
    Blank Secrets.h file to improve ease of use while maintaining confidentiality.

---
+++ Secrets.h
@@ -0,0 +1,13 @@
+#define TOPIC_T "__full-MQTT-topic__"
+#define OTA_HOSTNAME "__OTA-hostname__"
+
+// Number of seconds on power applied before device actually turns on. For power outages, restarts, etc...
+#define STARTUP_DELAY_SECONDS 900
+
+#define wifiScanTag "____ScanTag____"
+#define password "__wlan-password__"
+#define mqtt_server "__MQTT-server-ip__"
+
+uint16_t onData[__length-of-array__] = {__raw-ir-data-comma-separated-unsigned__};
+
+uint16_t offData[__length-of-array__] = {__raw-ir-data-comma-separated-unsigned__};
